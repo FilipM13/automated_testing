0c563d5e9943553da88c292a869ae31e1fd9829c
Filip Matejko <matejkofilip@gmail.com>
2022-04-25 14:24:48 +0200

    Fixing and expanding tests for get_ranks.

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,11 +4,13 @@ import bio_statistics.utils
 
 @pytest.mark.parametrize("test_input, expected", [
     (
-        [float(x) for x in [1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1234, 111]],
-        [
-            [0.0, 0.0], [1.0, 2.5], [1.0, 2.5], [1.0, 2.5], [1.0, 2.5], [2.0, 5.0], [3.0, 6.0], [4.0, 7.0],
-            [5.0, 8.0], [6.0, 9.0], [7.0, 10.0], [8.0, 11.0], [9.0, 12.0], [111.0, 13.0], [1234.0, 14.0]]
+        [float(x) for x in [0, 5, 6, 7, 8, -7, -1, 2, 1, 1]],
+        [[0, 1.0], [-1, 3.0], [1, 3.0], [1, 3.0], [2, 5.0], [5, 6.0], [6, 7.0], [7, 8.5], [-7, 8.5], [8, 10.0]]
+    ),
+    (
+        [],
+        []
     )
 ])
 def test_basic(test_input, expected):
-    assert bio_statistics.utils.get_rangs(test_input), expected
+    assert bio_statistics.utils.get_ranks(test_input) == expected, f'{bio_statistics.utils.get_ranks(test_input)} not equal to {expected}'
