e52049247e6af568960f33bab549fceb4d2a88ae
Filip Matejko <matejkofilip@gmail.com>
2022-04-25 14:23:55 +0200

    Fixing get_ranks function, adding TODOs for next functions.

--- src/bio_statistics/utils.py
+++ src/bio_statistics/utils.py
@@ -1,8 +1,22 @@
 import statistics
 
 
-def get_rangs(series: list[float]) -> list[list[float]]:
+# TODO: function: check_variance_normality
+# TODO: function: test_tStudent
+# TODO: function: test_UMannWhitney
+# TODO: function: test_Wilcoxon
+# TODO: function: test_KruskallWallis
+# TODO: function: test_Friedman
+# TODO: function: test_Anova
+# TODO: function: test_Anova_multivariate
 
+
+def get_ranks(series: list[float]) -> list[list[float]]:
+    """
+    Function calculating ranks for given series of values.
+    :param series: List of numeric values values
+    :return: List of pair of values where first element in pair is element from given input and second element is it's rang
+    """
     sorted_series = sorted(series, key=lambda x: abs(x))
     absolute_sorted_series = [abs(v) for v in sorted_series]
 
@@ -10,14 +24,14 @@ def get_rangs(series: list[float]) -> list[list[float]]:
 
     values_count = [absolute_sorted_series.count(v) for v in values]
 
-    values_range = [range(sum(values_count[:i]), sum(values_count[:i+1])) for i in range(len(values_count))]
+    values_range = [range(sum(values_count[:i])+1, sum(values_count[:i+1])+1) for i in range(len(values_count))]
 
-    rangs = [[statistics.mean(_l) for _ in _l] for _l in values_range]
+    ranks = [[statistics.mean(_l) for _ in _l] for _l in values_range]
     _tmp = []
-    for r in rangs:
+    for r in ranks:
         r = [float(_r) for _r in r]
         _tmp.extend(r)
 
-    rangs = [[s, t] for s, t in zip(sorted_series, _tmp)]
+    ranks = [[s, t] for s, t in zip(sorted_series, _tmp)]
 
-    return rangs
+    return ranks
