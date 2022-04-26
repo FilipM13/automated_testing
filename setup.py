354ed5ee0d725cd1957d834dd5cc19708c918be3
Filip Matejko <matejkofilip@gmail.com>
2022-04-26 19:20:14 +0200

    Adding variance normality checking and related test files. Changing pyproject to get coverage for tests. Formating setup file.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-  with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
 
-  setup(
-    name = 'bio_statistics',
-    description = 'Tools created for Statistics In Biomedicine course.',
-    author = 'Filip Matejko',
-    version='0.0.0',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/FilipM13/automated_testing',
-    license='MIT',
-    packages=['bio_statistics'],
-    install_requires=['statistics'],
-  )
+    setup(
+        name='bio_statistics',
+        description='Tools created for Statistics In Biomedicine course.',
+        author='Filip Matejko',
+        version='0.0.0',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url='https://github.com/FilipM13/automated_testing',
+        license='MIT',
+        packages=['bio_statistics'],
+        install_requires=['statistics'],
+    )
--- src/bio_statistics/utils.py
+++ src/bio_statistics/utils.py
@@ -1,7 +1,6 @@
 import statistics
 
-
-# TODO: function: check_variance_normality
+# TODO: class: CheckVarianceNormality, methods: fix Lilliefors, ShapiroWilk, KolmogorovSmirnov
 # TODO: function: test_tStudent
 # TODO: function: test_UMannWhitney
 # TODO: function: test_Wilcoxon
@@ -19,13 +18,9 @@ def get_ranks(series: list[float]) -> list[list[float]]:
     """
     sorted_series = sorted(series, key=lambda x: abs(x))
     absolute_sorted_series = [abs(v) for v in sorted_series]
-
     values = set(absolute_sorted_series)
-
     values_count = [absolute_sorted_series.count(v) for v in values]
-
     values_range = [range(sum(values_count[:i])+1, sum(values_count[:i+1])+1) for i in range(len(values_count))]
-
     ranks = [[statistics.mean(_l) for _ in _l] for _l in values_range]
     _tmp = []
     for r in ranks:
@@ -35,3 +30,59 @@ def get_ranks(series: list[float]) -> list[list[float]]:
     ranks = [[s, t] for s, t in zip(sorted_series, _tmp)]
 
     return ranks
+
+
+class CheckVarianceNormality:
+
+    @classmethod
+    def lilliefors(cls, series: list[float], critical_value: float) -> bool:
+
+        assert len(series) <= 1000, f'Series length is {len(series)}, max length for this test is 1000. Use different test.'
+
+        series = sorted(series)
+        series_mean = statistics.mean(series)
+        series_standard_deviation = statistics.stdev(series)
+        normalized_series = list(map(lambda si: (si - series_mean)/series_standard_deviation, series))
+
+        # TODO: get equation for comparison curve
+        compare = [0 for _ in range(len(normalized_series))]  # type: ignore
+        T = [abs(ns - c) for ns, c in zip(normalized_series, compare)]
+        T = min(T)  # type: ignore[arg-type]
+        rv = T < critical_value  # type: ignore[operator]
+        return rv
+
+    @classmethod
+    def shapiro_wilk(cls, series: list[float], critical_value: float) -> bool:
+        pass
+
+    @classmethod
+    def kolmogorov_smirnov(cls, series: list[float], critical_value: float) -> bool:
+        pass
+
+
+def test_tStudent():
+    pass
+
+
+def test_UMannWhitney():
+    pass
+
+
+def test_Wilcoxon():
+    pass
+
+
+def test_KruskallWallis():
+    pass
+
+
+def test_Friedman():
+    pass
+
+
+def test_Anova():
+    pass
+
+
+def test_Anova_multivariate():
+    pass
