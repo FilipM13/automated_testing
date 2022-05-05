b5f86f68408c6a15c97ed0d67e9f6c26dbc00979
Filip Matejko <matejkofilip@gmail.com>
2022-05-05 23:58:59 +0200

    Simplyfying test case for get_ranks.

--- tests/test_bio_statistics/test_get_ranks.py
+++ tests/test_bio_statistics/test_get_ranks.py
@@ -5,10 +5,10 @@ from bio_statistics.utils import get_ranks
 # test basic behaviour of function with regular cases and edge cases
 @pytest.mark.parametrize("test_input, expected", [
     (  # test case 1
-        [float(x) for x in [0, 5, 6, 7, 8, -7, -1, 2, 1, 1]],
+        [0, 5, 6, 7, 8, -7, -1, 2, 1, 1],
         [[0, 1.0], [-1, 3.0], [1, 3.0], [1, 3.0], [2, 5.0], [5, 6.0], [6, 7.0], [7, 8.5], [-7, 8.5], [8, 10.0]]
     ),
-    (
+    (  # test case 2
         [],
         []
     )
