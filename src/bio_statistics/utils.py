6cdf395fbe330f882fca37e4e8d3a53261c6c784
Filip Matejko <matejkofilip@gmail.com>
2022-05-05 23:29:36 +0200

    Adding U Mann Whitney test with basic test case.

--- src/bio_statistics/utils.py
+++ src/bio_statistics/utils.py
@@ -34,7 +34,7 @@ def get_ranks(series: list[float]) -> list[list[float]]:
 
     ranks = [[s, t] for s, t in zip(sorted_series, _tmp)]  # create list of pairs (value, rank)
 
-    return ranks
+    return ranks  # return test result
 
 
 class CheckVarianceNormality:
@@ -60,7 +60,38 @@ def tStudent():
 
 
 def UMannWhitney(series1: list[float], series2: list[float], critical_value: float) -> bool:
-    pass
+    """
+    Reject H0 hypothesis if return value is True.
+    :param series1:
+    :param series2:
+    :param critical_value:
+    :return:
+    """
+
+    # input data validation
+    assert len(series1) <= 20 and len(series2) <= 20 or len(series1) > 20 and len(series2) > 20, 'Both series must be in the same count group (both <= 20 or both >20).'  # noqa: E501
+    assert all([isinstance(v, (float, int)) for v in series1]), 'All values must be numeric (int or float).'
+    assert all([isinstance(v, (float, int)) for v in series2]), 'All values must be numeric (int or float).'
+    assert isinstance(critical_value, (float, int)), 'Critical value must be numeric (int or float).'
+
+    # test steps
+    series1_ranks = get_ranks(series1)  # get ranks for both series
+    series2_ranks = get_ranks(series2)
+    sum_series1_ranks = sum([r[1] for r in series1_ranks])  # sum ranks for both series
+    sum_series2_ranks = sum([r[1] for r in series2_ranks])
+    n1 = len(series1)  # get length of both series
+    n2 = len(series2)
+    if n1 <= 20 and n2 <= 20:  # calculate statistic if both series are not larger than 20
+        u1 = n1 * n2 + n1 * (n1 + 1) / 2 - sum_series1_ranks
+        u2 = n1 * n2 + n2 * (n2 + 1) / 2 - sum_series2_ranks
+        u = min(u1, u2)
+    elif n1 > 20 and n2 > 20:  # calculate statistic if both series are larger than 20
+        n = n1 + n2
+        _top = sum_series1_ranks - sum_series2_ranks - (n1 - n2) * (n + 1) / 2
+        _bottom = (n1 * n2 * (n+1) / 3) ** 0.5
+        u = _top / _bottom
+    rv = u < critical_value  # compare statistic with critical value
+    return rv  # return result
 
 
 def Wilcoxon(series1: list[float], series2: list[float], critical_value: float = 0.05) -> bool:
---
+++ tests/test_bio_statistics/test_umannwhitney.py
@@ -0,0 +1,20 @@
+import pytest
+from bio_statistics.utils import UMannWhitney
+
+
+@pytest.mark.parametrize("x1, x2, cv, result", [
+    (  # test case 1
+        [1, 2, 3, 4],
+        [2, 3, -1, 5],
+        0.5,
+        False
+    ),
+    (  # test case 2
+        [1, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4],
+        [2, 3, -1, 5, 3, -1, 5, 3, -1, 5, 3, -1, 5, 3, -1, 5, -1, 5, 3, -1, 5, 3, -1, 5, 3, -1, 5, 3, -1, 5],
+        0.5,
+        True
+    )
+])
+def test_basic(x1, x2, cv, result):
+    assert UMannWhitney(x1, x2, cv) == result
