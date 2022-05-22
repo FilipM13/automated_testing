09688f2bc298ef025e511ac78400f94c52b26223
Filip Matejko <matejkofilip@gmail.com>
2022-05-22 19:42:03 +0200

    Adding critical value table for UMannWhitney test and function for getting critical value. Might need refactor for using json file instead of python dictionaries.

--- src/bio_statistics/utils.py
+++ src/bio_statistics/utils.py
@@ -1,11 +1,45 @@
 import statistics
+from typing import Any
+from bio_statistics import critical_value_tables
 
-# TODO: function: tStudent
+# TODO: add files with crtical value tables
+# TODO: remove critical_value parameter from test functions
 # TODO: function: Friedman
 # TODO: class: CheckVarianceNormality, methods: Lilliefors, ShapiroWilk, KolmogorovSmirnov
 # TODO: function: KruskallWallis
 # TODO: function: Anova
 # TODO: function: Anova_multivariate
+
+
+def _get_critical_value(test: str, **kwargs: Any) -> float:
+    """
+    Get critical value for test from source UMannWhitney_table.
+    :param test: name of test
+    :param kwargs: parameters to get value from UMannWhitney_table
+    :return: critical value
+    """
+
+    sources = [
+        'UMannWhitney'
+    ]
+
+    assert test in sources, 'Unavailable test.'
+
+    if test == 'UMannWhitney':
+        alfa = kwargs['alfa']
+
+        n1 = kwargs['n1']
+        if n1 <= 3:
+            n1 = 3
+
+        n2 = kwargs['n2']
+        if n2 <= 3:
+            n2 = 3
+
+        rv = critical_value_tables.UMannWhitney_table[alfa][n1][n2]  # noqa: F841
+        return 0.5  # temporary, replace with rv
+
+    return 0.5
 
 
 def _lower_dict_sum(series: dict[float, int], k: float) -> int:
@@ -49,6 +83,7 @@ def get_ranks(series: list[float]) -> list[list[float]]:
     return ranks
 
 
+# disabled
 class CheckVarianceNormality:
     """
     Temporarily disabled.
@@ -66,7 +101,7 @@ class CheckVarianceNormality:
         pass
 
 
-# TODO: adding paired samples and independent samples version of t student
+# add _get_critical_value functionality
 def tStudent(series1: list[float], series2: list[float], critical_value: float, independent_samples: bool = True) -> bool:
     """
     Reject H0 hypothesis if return value is True.
@@ -124,7 +159,7 @@ def tStudent(series1: list[float], series2: list[float], critical_value: float, 
     return rv  # type: ignore[no-any-return]
 
 
-def UMannWhitney(series1: list[float], series2: list[float], critical_value: float) -> bool:
+def UMannWhitney(series1: list[float], series2: list[float], alfa: float) -> bool:
     """
     Reject H0 hypothesis if return value is True.
     :param series1:
@@ -134,10 +169,10 @@ def UMannWhitney(series1: list[float], series2: list[float], critical_value: flo
     """
 
     # input data validation
-    assert len(series1) <= 20 and len(series2) <= 20 or len(series1) > 20 and len(series2) > 20, 'Both series must be in the same count group (both <= 20 or both >20).'  # noqa: E501
+    assert (len(series1) <= 20 and len(series2) <= 20) or (len(series1) > 20 and len(series2) > 20), 'Both series must be in the same count group (both <= 20 or both >20).'  # noqa: E501
     assert all([isinstance(v, (float, int)) for v in series1]), 'All values must be numeric (int or float).'
     assert all([isinstance(v, (float, int)) for v in series2]), 'All values must be numeric (int or float).'
-    assert isinstance(critical_value, (float, int)), 'Critical value must be numeric (int or float).'
+    assert isinstance(alfa, (float, int)), 'Critical value must be numeric (int or float).'
 
     # test steps
     series1_ranks = get_ranks(series1)  # get ranks for both series
@@ -146,19 +181,27 @@ def UMannWhitney(series1: list[float], series2: list[float], critical_value: flo
     sum_series2_ranks = sum([r[1] for r in series2_ranks])
     n1 = len(series1)  # get length of both series
     n2 = len(series2)
-    if n1 <= 20 and n2 <= 20:  # calculate statistic if both series are not larger than 20
+
+    # calculate statistic if both series are not larger than 20
+    if n1 <= 20 and n2 <= 20:
         u1 = n1 * n2 + n1 * (n1 + 1) / 2 - sum_series1_ranks
         u2 = n1 * n2 + n2 * (n2 + 1) / 2 - sum_series2_ranks
         u = min(u1, u2)
-    elif n1 > 20 and n2 > 20:  # calculate statistic if both series are larger than 20
+        critical_value = _get_critical_value(test='UMannWhitney', alfa=alfa, n1=n1, n2=n2)
+
+    # calculate statistic if both series are larger than 20
+    elif n1 > 20 and n2 > 20:
         n = n1 + n2
         _top = sum_series1_ranks - sum_series2_ranks - (n1 - n2) * (n + 1) / 2
         _bottom = (n1 * n2 * (n+1) / 3) ** 0.5
         u = _top / _bottom
+        critical_value = 0.5
+
     rv = u < critical_value  # compare statistic with critical value
     return rv  # return result
 
 
+# add _get_critical_value functionality
 def Wilcoxon(series1: list[float], series2: list[float], critical_value: float = 0.05) -> bool:
     """
     Reject H0 hypothesis if return value is True.
